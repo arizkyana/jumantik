10b397b5ba54790e6cf129d123a552a47abf9d1b
arizkyana <agung.rizkyana@mail.unpas.ac.id>
2017-11-06 11:37:54 +0700

    Setup FCM Server

--- app/Utils/FCM.php
+++ app/Utils/FCM.php
@@ -9,17 +9,43 @@
 namespace App\Utils;
 
 
+use Illuminate\Support\Facades\Config;
+use GuzzleHttp\Client;
+
 class FCM
 {
+    private $FCM_SERVER_KEY;
+    private $FCM_SENDER_KEY;
+    private $FCM_URL_LEGACY;
+
     public function __construct()
     {
+        $this->FCM_SENDER_KEY = Config::get('fcm.FCM_SERVER_KEY');
+        $this->FCM_SENDER_KEY = Config::get('fcm.FCM_SENDER_KEY');
+        $this->FCM_URL_LEGACY = Config::get('fcm.FCM_URL_LEGACY');
     }
 
-    public function broadcast(array $receivers){
+    public function send_messages(array $receivers, $title, $body){
 
+        $body = json_encode([
+            'registration_ids' => json_encode($receivers),
+            'notification' => [
+                'title' => $title,
+                'body' => $body
+            ]
+        ]);
+
+        $client = new Client([
+            'headers' => [
+                'Authorization' => 'key=' . $this->FCM_SERVER_KEY,
+                'Content-type' => 'application/json'
+            ]
+        ]);
+
+        $client->request('POST', $this->FCM_URL_LEGACY, [
+            'body' => $body
+        ]);
     }
 
-    public function single($receiver){
 
-    }
 }
---
+++ config/fcm.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: agung
+ * Date: 11/6/2017
+ * Time: 11:21
+ */
+
+return [
+    'FCM_SERVER_KEY' => env('FCM_SERVER_KEY', 'AIzaSyD2tMDNFjhzjnA5pft6rE1tDcv-XnMxHTc'),
+    'FCM_SENDER_KEY' => env('FCM_SENDER_KEY', '613796250855'),
+    'FCM_URL_LEGACY' => env('FCM_URL_LEGACY', 'https://fcm.googleapis.com/fcm/send')
+];
